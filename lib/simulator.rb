89447de579ef99bc10ab56c885766f6e59e06e46
Jones Agyemang <mightyj@hotmail.co.uk>
2026-09-22 19:56:24 +0100

    Executes subsequent commands after robot is placed

--- lib/simulator.rb
+++ lib/simulator.rb
@@ -2,10 +2,33 @@
 
 # Runs the given
 class Simulator
+  attr_reader :commands, :robot
+
   def initialize(commands, robot)
     @commands = commands
     @robot = robot
   end
 
-  def run; end
+  def run
+    placed = false
+    output = []
+    commands.each do |command|
+      cmd, args = command.split(' ')
+
+      placed = true if cmd == 'PLACE'
+      next unless placed
+
+      case cmd
+      when 'PLACE'
+        x, y, orientation = args.split(',')
+        robot.place(x: x.to_i, y: y.to_i, orientation: )
+      when 'MOVE'
+        robot.move
+      when 'REPORT'
+        output << robot.report
+      end
+    end
+
+    output
+  end
 end
--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -3,15 +3,37 @@
 require_relative '../lib/simulator'
 
 RSpec.describe Simulator do
+  subject(:simulator) { described_class.new(commands, robot) }
+
   describe '#run' do
     context 'when command set has no valid PLACE command' do
+      let(:commands) { %w[MOVE REPORT] }
+      let(:robot) { spy('robot') }
+
       it 'discards all commands' do
-        commands = %w[MOVE REPORT]
-        robot = spy('robot')
-        simulator = described_class.new(commands, robot)
         simulator.run
 
         expect(robot).not_to have_received(:move)
+        expect(robot).not_to have_received(:report)
+      end
+    end
+
+    context 'when command has a valid PLACE command' do
+      context 'when PLACE at the beginning of the command sequence' do
+        let(:commands) do
+          [
+            'PLACE 0,0,NORTH',
+            'MOVE',
+            'REPORT'
+          ]
+        end
+        let(:robot) { Robot.new }
+
+        it 'successfully executes all subsequent commands' do
+          simulation_output = simulator.run
+
+          expect(simulation_output).to eq(['0,1,NORTH'])
+        end
       end
     end
   end
