db68879b7118b2cbbdb6e6aa818c8c98bee30474
Jones Agyemang <mightyj@hotmail.co.uk>
2026-09-23 06:16:48 +0100

    Extract error classes

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class InvalidMoveError < StandardError; end
-class InvalidOrientationError < StandardError; end
+require_relative '../lib/errors/invalid_move_error'
+require_relative '../lib/errors/invalid_orientation_error'
 
 # Bot that facilitates exploration
 class Robot
