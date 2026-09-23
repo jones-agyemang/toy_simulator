09c5ccdd6e33207af1f8c1cfc378ead5b00f4c8b
Jones Agyemang <mightyj@hotmail.co.uk>
2026-09-23 11:58:53 +0100

    Add distinct move classes for the cardinal points

---
+++ lib/move/west.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Move
+  # Moves one unit to the West
+  class West
+    attr_accessor :robot
+
+    def initialize(robot)
+      @robot = robot
+    end
+
+    def self.call(robot)
+      return unless robot.table.within_bounds?(robot.x - 1, robot.y)
+
+      new(robot)
+      robot.x -= 1
+    end
+  end
+end
---
+++ spec/move/north_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/robot'
+require_relative '../../lib/move/north'
+
+RSpec.describe Move::North do
+  subject(:invoke_movement) { described_class.call(robot) }
+
+  describe '.call' do
+    let(:robot) { instance_double(Robot) }
+    let(:table) { instance_double(Table) }
+
+    before do
+      allow(table).to receive(:within_bounds?).and_return(true)
+      allow(robot).to receive_messages(table: table, x: 0, y: 0)
+      allow(robot).to receive(:y=)
+      allow(robot).to receive(:x=)
+
+      invoke_movement
+    end
+
+    it 'moves robot up' do
+      expect(robot).to have_received(:y=).with(1)
+    end
+
+    it 'does not move robot down' do
+      expect(robot).not_to have_received(:y=).with(-1)
+    end
+
+    it 'does not move robot to the left' do
+      expect(robot).not_to have_received(:x=).with(-1)
+    end
+
+    it 'does not move robot to the right' do
+      expect(robot).not_to have_received(:x=).with(1)
+    end
+  end
+end
